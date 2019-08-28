290ac18e336a9d3a66d90f1895360b747d6dee38
yumseungil <54626888+yumseungil@users.noreply.github.com>
2019-08-28 20:59:58 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php
-/**
- * @file
- * User has successfully authenticated with Twitter. Access tokens saved to session and DB.
- */
+ini_set('display_errors', 1);
+require_once('TwitterAPIExchange.php');
 
-/* Load required lib files. */
-session_start();
-require_once('twitteroauth/twitteroauth.php');
-require_once('config.php');
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "",
+    'oauth_access_token_secret' => "",
+    'consumer_key' => "",
+    'consumer_secret' => ""
+);
 
-/* If access tokens are not available redirect to connect page. */
-if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
-    header('Location: ./clearsessions.php');
-}
-/* Get user access tokens out of the session. */
-$access_token = $_SESSION['access_token'];
+/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+$url = 'https://api.twitter.com/1.1/blocks/create.json';
+$requestMethod = 'POST';
 
-/* Create a TwitterOauth object with consumer/user tokens. */
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+/** POST fields required by the URL above. See relevant docs as above **/
+$postfields = array(
+    'screen_name' => 'usernameToBlock', 
+    'skip_status' => '1'
+);
 
-/* If method is set change API call made. Test is called by default. */
-$content = $connection->get('account/verify_credentials');
+/** Perform a POST request and echo the response **/
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setPostfields($postfields)
+             ->performRequest();
 
-/* Some example calls */
-//$connection->get('users/show', array('screen_name' => 'abraham'));
-//$connection->post('statuses/update', array('status' => date(DATE_RFC822)));
-//$connection->post('statuses/destroy', array('id' => 5437877770));
-//$connection->post('friendships/create', array('id' => 9436992));
-//$connection->post('friendships/destroy', array('id' => 9436992));
-
-/* Include HTML to display on the page */
-include('html.inc');
+/** Perform a GET request and echo the response **/
+/** Note: Set the GET field BEFORE calling buildOauth(); **/
+$url = 'https://api.twitter.com/1.1/followers/ids.json';
+$getfield = '?screen_name=J7mbo';
+$requestMethod = 'GET';
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
